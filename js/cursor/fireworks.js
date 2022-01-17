9dbf23acf7f2ec49b2ba13ac518b04a4b2c61e9b
YfxHit <2878799892@qq.com>
2022-01-17 22:15:34 +0800

    Site updated: 2022-01-17 22:15:34

--- js/cursor/fireworks.js
+++ js/cursor/fireworks.js
@@ -110,6 +110,7 @@ class Circle {
     handlePageHide() {
       this.booms = []
       this.running = false
+      // 简单修改，清理停留不动的烟火特效
       this.computerContext.clearRect(0, 0, this.globalWidth, this.globalHeight)
       this.renderContext.clearRect(0, 0, this.globalWidth, this.globalHeight)
     }
